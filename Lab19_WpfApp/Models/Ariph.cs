6f0a11035664762d6cc355bfc7a739904da87478
avolodin1479@gmail.com <avolodin1479@gmail.com>
2022-01-12 12:39:47 +0600

    Добавьте файлы проекта.

---
+++ Lab19_WpfApp/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab19_WpfApp.Models
+{
+    static class Ariph
+    {
+        public static double Add(double a) => 2*a*Math.PI; 
+    }
+}
---
+++ Lab19_WpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Lab19_WpfApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Lab19_WpfApp.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+       
+        private double len;
+        public double Len
+        {
+            get => len;
+            set
+            {
+                len = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Len = Ariph.Add(Radius);
+        }
+        private bool CanAddCommandExecute(object p)
+        {
+            if (Radius != null)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+    }
+}
